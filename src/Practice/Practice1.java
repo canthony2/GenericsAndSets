0b67274f3688379d0e3b47601454cc6254cf1182
canthony2 <canthony2@L11714042777.wctc.msft>
2017-04-17 15:10:51 -0500

    Learning about lists

--- src/Practice/Practice1.java
+++ src/Practice/Practice1.java
@@ -1,5 +1,7 @@
 package Practice;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author canthony2
@@ -7,7 +9,28 @@ package Practice;
 public class Practice1 {
     public static void main(String[] args) {
         
+//        Manual Boxing and Unboxing
+//        Integer x = new Integer(1);
+//        int primitive = x.intValue();
+
+        //Automatic Boxing and Unboxing
+        Integer x = 1;
+        int primitve = x;
         
+        //Type Wrapper classes
+        //int ---> Integer
+        //long ---> Long
+        //double ---> Double
+        //etc...
         
+        //Wrong way
+        ArrayList list = new ArrayList();
+        
+        list.add("hello");
+        list.add(1);
+        Object value = list.get(1);
+        
+
+
     }
 }
